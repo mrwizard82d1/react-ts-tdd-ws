ac8c6c29e073e02e9492f1892060d209d53fa32d
Larry Jones <larry.jones@reveal-energy.com>
2022-07-28 19:13:04 -0500

    Remove unused imports (via Optimize imports)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-      <div>
-        <h1>Hello React</h1>
-      </div>
+    <div>
+      <h1>Hello React</h1>
+    </div>
   );
 }
 
