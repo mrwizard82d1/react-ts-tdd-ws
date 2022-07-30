493bc2ff82ded203b8886ec0448d6a11ae62399a
Larry Jones <larry.jones@reveal-energy.com>
2022-07-30 10:53:24 -0500

    Remove added, extraneous, illustrative code
    Previous commits illustrated aspects of WebStorm and testing, but were
    not really needed for the "Web app".

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,9 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App, {label} from "./App";
+import App from "./App";
 
 test("renders hello world", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello React/i);
+  const linkElement = screen.getByText(/hello react/i);
   expect(linkElement).toBeInTheDocument();
-
-  const actual = 1;
-  const expected = 1;
-  expect(actual).toEqual(expected);
 });
-
-test("generates correct label", () => {
-  expect(label("React")).toEqual("Hello REACT")
-})
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 
-
-export function label(toGreet: string) {
-  return `Hello ${toGreet.toUpperCase()}`;
-}
-
 function App() {
   return (
     <div>
-      <h1>{label("react")}</h1>
+      <h1>{"Hello React"}</h1>
     </div>
   );
 }
