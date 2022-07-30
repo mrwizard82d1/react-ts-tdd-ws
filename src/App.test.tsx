5fe69d45daa33c92363cce8d5f7a739740bd83fe
Larry Jones <larry.jones@reveal-energy.com>
2022-07-30 00:23:39 -0500

    Introduce `label` function

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App, {label} from "./App";
 
 test("renders hello world", () => {
   render(<App />);
-  const linkElement = screen.getByText(/hello react/i);
+  const linkElement = screen.getByText(/Hello React/i);
   expect(linkElement).toBeInTheDocument();
 
   const actual = 1;
   const expected = 1;
   expect(actual).toEqual(expected);
 });
+
+test("generates correct label", () => {
+  expect(label()).toEqual("Hello React")
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+
+export function label() {
+    return "Hello React"
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>{label()}</h1>
     </div>
   );
 }
