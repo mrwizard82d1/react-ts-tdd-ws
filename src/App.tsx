9a99c8e19010c424414b7d8fea92a0c955552185
Larry Jones <larry.jones@reveal-energy.com>
2022-07-30 17:02:38 -0500

    Use *optional* name parameter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export type HeadingProps = { name: string };
+export type HeadingProps = { name?: string };
 
 export function Heading({name = "React"}: HeadingProps) {
   return <h1>Hello {name}</h1>;
