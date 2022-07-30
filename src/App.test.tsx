0ae2cb23c1cf0c100e6fe87eb13c3b7f8143d668
Larry Jones <larry.jones@reveal-energy.com>
2022-07-30 17:12:43 -0500

    Extarct Heading tests into own file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,22 +1,9 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import {Heading} from "./Heading";
 
 test("renders hello world", () => {
   render(<App />);
   const linkElement = screen.getByText(/hello react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test("renders heading", () => {
-  render(<Heading />);
-  const linkElement = screen.getByText(/hello react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test("render heading with an argument", () => {
-  render(<Heading name={`World`}/>);
-  const linkElement = screen.getByText(/hello world/i);
   expect(linkElement).toBeInTheDocument();
 });
