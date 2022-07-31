1e5ff4b5614cfe52dfe55c45a4719b32bbb04f64
Larry Jones <larry.jones@reveal-energy.com>
2022-07-30 23:52:59 -0500

    Use Counter component in App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,9 @@ test("renders hello world", () => {
   const linkElement = screen.getByText(/hello react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("renders count component with label", () => {
+  render(<App />);
+  const linkElement = screen.getByLabelText("Current");
+  expect(linkElement).toBeInTheDocument();
+});
