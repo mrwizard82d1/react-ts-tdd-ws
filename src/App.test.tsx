b04ec6d60ba7782d97811dcac1a4755c096f9715
Larry Jones <larry.jones@reveal-energy.com>
2022-07-31 11:47:51 -0500

    Test default counter in app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,12 +4,18 @@ import App from "./App";
 
 test("renders hello world", () => {
   render(<App />);
-  const linkElement = screen.getByText(/hello react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/hello react/i);
+  expect(heading).toBeInTheDocument();
 });
 
 test("renders count component with label", () => {
   render(<App />);
-  const linkElement = screen.getByLabelText("Current");
-  expect(linkElement).toBeInTheDocument();
+  const counter  = screen.getByLabelText("Current");
+  expect(counter).toBeInTheDocument();
+});
+
+test("renders counter starting at zero", () => {
+  render(<App />);
+  const counter = screen.getByRole("counter");
+  expect(counter.textContent).toEqual("0");
 });
