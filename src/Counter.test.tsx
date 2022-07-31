71bbf8979bb1b5025a8dced32ded2ad8ed47dfda
Larry Jones <larry.jones@reveal-energy.com>
2022-07-31 17:53:12 -0500

    Add "mock" function for handler

--- src/Counter.test.tsx
+++ src/Counter.test.tsx
@@ -1,40 +1,44 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import Counter from "./Counter";
 import userEvent from "@testing-library/user-event";
 
 test("should render a label and a counter", () => {
-  render(<Counter count={0} />);
-  const label = screen.getByLabelText("Count");
-  expect(label).toBeInTheDocument();
+    const handler = jest.fn();  // Essentially, a mock
+    render(<Counter count={0} onCounterIncrease={handler}/>);
+    const label = screen.getByLabelText("Count");
+    expect(label).toBeInTheDocument();
 
-  const counter = screen.getByRole("counter");
-  expect(counter).toBeInTheDocument();
+    const counter = screen.getByRole("counter");
+    expect(counter).toBeInTheDocument();
 });
 
 test("should render a counter with a custom label", () => {
-  render(<Counter label={`Current`} count={0} />);
-  const label = screen.getByLabelText("Current");
-  expect(label).toBeInTheDocument();
+    const handler = jest.fn();  // Essentially, a mock
+    render(<Counter label={`Current`} count={0} onCounterIncrease={handler}/>);
+    const label = screen.getByLabelText("Current");
+    expect(label).toBeInTheDocument();
 
-  const counter = screen.getByRole("counter");
-  expect(counter).toBeInTheDocument();
+    const counter = screen.getByRole("counter");
+    expect(counter).toBeInTheDocument();
 });
 
 test("should increment counter on click", () => {
-  render(<Counter count={0} />);
-  const counter = screen.getByRole("counter");
+    const handler = jest.fn();  // Essentially, a mock
+    render(<Counter count={0} onCounterIncrease={handler}/>);
+    const counter = screen.getByRole("counter");
 
-  expect(counter).toHaveTextContent(/^0$/);
-  fireEvent.click(counter);
-  expect(counter).toHaveTextContent(/^1$/);
+    expect(counter).toHaveTextContent(/^0$/);
+    fireEvent.click(counter);
+    expect(counter).toHaveTextContent(/^1$/);
 });
 
 test("should increment counter by 10 on shift click", () => {
-  render(<Counter count={0} />);
-  const counter = screen.getByRole("counter");
+    const handler = jest.fn();  // Essentially, a mock
+    render(<Counter count={0} onCounterIncrease={handler}/>);
+    const counter = screen.getByRole("counter");
 
-  expect(counter).toHaveTextContent(/^0$/);
-  userEvent.click(counter, { shiftKey: true });
-  expect(counter).toHaveTextContent(/^10$/);
+    expect(counter).toHaveTextContent(/^0$/);
+    userEvent.click(counter, {shiftKey: true});
+    expect(counter).toHaveTextContent(/^10$/);
 });
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,23 +1,31 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 
-export type CounterProps = { label?: string; count: number };
+export type CounterProps = {
+  label?: string;
+  count: number;
+  onCounterIncrease: (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void;
+};
 
 const initialState = { count: 0 };
 export type CounterState = Readonly<typeof initialState>;
 
-const Counter = ({ label = "Count", count } : CounterProps) => {
+const Counter = ({ label = "Count", count }: CounterProps) => {
   return (
-      <div>
-        <label htmlFor="counter" style={{ margin: "5px" }}>
-          {label}
-        </label>
-        <button id="counter" role="counter"
-                // onClick=incrementCounter
-        >
-          {count}
-        </button>
-      </div>
+    <div>
+      <label htmlFor="counter" style={{ margin: "5px" }}>
+        {label}
+      </label>
+      <button
+        id="counter"
+        role="counter"
+        // onClick=incrementCounter
+      >
+        {count}
+      </button>
+    </div>
   );
-}
+};
 
 export default Counter;
