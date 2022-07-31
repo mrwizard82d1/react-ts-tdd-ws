f984244f98493ed9db2dd222c029fb2b214d49e4
Larry Jones <larry.jones@reveal-energy.com>
2022-07-31 17:40:57 -0500

    Remove tests for absent feature
    Previous commits allowed callers to set the initial count. Since the
    component no longer has state, the call has the responsibility for
    supplying the current count.

--- src/Counter.test.tsx
+++ src/Counter.test.tsx
@@ -21,20 +21,6 @@ test("should render a counter with a custom label", () => {
   expect(counter).toBeInTheDocument();
 });
 
-test("should start counting at zero (0)", () => {
-  render(<Counter count={0} />);
-  const counterValue = screen.getByRole("counter");
-
-  expect(counterValue).toHaveTextContent("0");
-});
-
-test("should start counting at a specified value", () => {
-  render(<Counter count={10} />);
-  const counter = screen.getByRole("counter");
-
-  expect(counter).toHaveTextContent("10");
-});
-
 test("should increment counter on click", () => {
   render(<Counter count={0} />);
   const counter = screen.getByRole("counter");
