68e431bf1229ff50b2179865f3a593bd12cb4215
Larry Jones <larry.jones@reveal-energy.com>
2022-07-31 09:25:02 -0500

    Added component state with attribute count

--- src/Counter.test.tsx
+++ src/Counter.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Counter from "./Counter";
+import counter from "./Counter";
 
 test("should render a label and a counter", () => {
   render(<Counter />);
@@ -19,3 +20,10 @@ test("should render a counter with a custom label", () => {
   const counter = screen.getByRole("counter");
   expect(counter).toBeInTheDocument();
 });
+
+test("should start counting at zero (0)", () => {
+  render(<Counter />);
+  const counterValue = screen.getByRole("counter");
+
+  expect(counterValue).toHaveTextContent("0");
+})
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 
 export type CounterProps = { label?: string };
+export type CounterState = { count: number };
 
-class Counter extends Component<CounterProps> {
+class Counter extends Component<CounterProps, CounterState> {
+  state: CounterState = { count: 0 };
+
   render() {
     const { label = "Count" } = this.props;
+    const { count } = this.state;
+
     return (
       <div>
         <label htmlFor="counter">{label}</label>
-        <input type="number" id="counter" role="counter" />
+        <output id="counter" role="counter">
+          {count}
+        </output>
       </div>
     );
   }
