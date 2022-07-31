67b4d64c5e1a8749ad464ab7cfb9653070cfe6d9
Larry Jones <larry.jones@reveal-energy.com>
2022-07-31 18:11:55 -0500

    All tests now pass

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Heading } from "./Heading";
 import Counter from "./Counter";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => setCount(count + 1);
   return (
     <div>
       <Heading />
-      <Counter label="Current" count={0} />
+      <Counter
+        label="Current"
+        count={count}
+        onCounterIncrease={incrementCounter}
+      />
     </div>
   );
 }
