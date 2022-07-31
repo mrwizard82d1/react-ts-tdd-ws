b093dea973f86913cae246140183346761e293b1
Larry Jones <larry.jones@reveal-energy.com>
2022-07-30 23:19:50 -0500

    Address "destructing render; use screen" issue

--- src/Counter.test.tsx
+++ src/Counter.test.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import {render} from "@testing-library/react"
+import { render, screen } from "@testing-library/react";
 import Counter from "./Counter";
 
 test("should render a label and a counter", () => {
-    const {getByLabelText, getByRole} = render(<Counter/>);
-    const label = getByLabelText("Count");
-    expect(label).toBeInTheDocument();
+  render(<Counter />);
+  const label = screen.getByLabelText("Count");
+  expect(label).toBeInTheDocument();
 
-    const counter = getByRole("counter");
-    expect(counter).toBeInTheDocument();
-})
+  const counter = screen.getByRole("counter");
+  expect(counter).toBeInTheDocument();
+});
